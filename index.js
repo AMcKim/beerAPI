97512833995070aaa1db03afad80e44cc5d89103
McKim <AAM1022@daugherty.com>
2019-10-02 09:19:40 -0500

    added catch all route

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.use('/', (req, res) => {
+    console.log('Something hit me!');
+    res.send(`<h1>Welcome to the beer API</h1>`);
+})
+
+//listen without specifying a port number
+
+const port = process.env.PORT || 4444;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
