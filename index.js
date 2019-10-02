3b74651f59f85f33eb321093fa821c787188d7c9
McKim <AAM1022@daugherty.com>
2019-10-02 09:37:15 -0500

    added beer router and imported mongoose

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const beerRouter = require('./routes/beerRouter');
+const mongoose = require('mongoose');
 
 
+//handles only get requests for 
+
+
+app.use('/beers', beerRouter)
+
+//catches all requests
 app.use('/', (req, res) => {
     console.log('Something hit me!');
     res.send(`<h1>Welcome to the beer API</h1>`);
